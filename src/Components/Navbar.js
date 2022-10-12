b94c85e0a0f72a20efa59d0736aa49a1ee785150
Aditya <aditya.coder52you@gmail.com>
2022-10-12 13:03:57 +0530

    Marked Issue page as active

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,12 +18,12 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <div>
-                                <li className="nav-item active">
+                                <li className="nav-item ">
                                     <a className="nav-link" href="#"><CodeIcon />Code <span className="sr-only">(current)</span></a>
                                 </li>
                             </div>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#"><PlayCircleOutlineIcon />Issues</a>
+                            <li className="nav-item active" >
+                                <a style={{fontWeight:"bold" }} className="nav-link" href="#"><PlayCircleOutlineIcon />Issues</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#"><PlayCircleOutlineIcon />Pull Requests</a>
